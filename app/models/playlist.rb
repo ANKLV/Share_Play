8cd3036d3047d8e62fbd03ba31fc4eecf2e20c5c
andrew-lukyanov <nefesh.samael@gmail.com>
2021-04-15 11:08:12 +0300

    Refactoring

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Playlist < ApplicationRecord
-  has_many :playlist_tracks
+  has_many :playlist_tracks, dependent: :destroy
   has_many :tracks, through: :playlist_tracks
 
   validates :title, presence: true
