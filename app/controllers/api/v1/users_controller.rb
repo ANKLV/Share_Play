1f722380ab22bd6806ab3d91709c74a70b252bb5
andrew-lukyanov <nefesh.samael@gmail.com>
2021-05-07 17:25:25 +0300

    Rubocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,28 +3,19 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      def new
-        @user = User.new
-      end
-
       def create
-        @user = User.create(user_auth_params)
-        session[:user_id] = @user.id
-        if @user.valid?
+        @user = User.new(user_params)
+        if @user.save
           render json: @user, status: :created
         else
-          redirect_to '/users/new', alert: 'Invalid email or password'
+          render json: @user.errors, status: :unprocessable_entity
         end
       end
 
       private
 
       def user_params
-        params.require(:user).permit(:nickname)
-      end
-
-      def user_auth_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :nickname, :password)
       end
     end
   end
