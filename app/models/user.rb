fa687c7ef695045f114e573cf03276c1405e090b
andrew-lukyanov <nefesh.samael@gmail.com>
2021-05-07 15:44:44 +0300

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_secure_password :password_digest, validations: true
+  has_many :tracks
+  has_many :playlists
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  validates :password_digest, presence: true, length: { minimum: 6, maximum: 50 }, unless: :persisted?
+  validates :email, presence: true, length: {
+    minimum: 5, maximum: 50
+  }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+end
