b6f1e34922523c512d7321b9055b9b136c8da059
andrew-lukyanov <nefesh.samael@gmail.com>
2021-05-11 11:20:50 +0300

    Validations for email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user do
     nickname { 'MyString' }
-    email { FFaker::Name.unique.name }
+    email { FFaker::Internet.email }
     password { '123456' }
   end
 end
